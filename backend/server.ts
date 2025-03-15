74ecbf269f2cfedd8c6ff117aad25e29b07fed68
Vakul Keshav <95099652+Keshavvakul@users.noreply.github.com>
2025-03-16 01:55:10 +0530

    Update server.ts

--- backend/server.ts
+++ backend/server.ts
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({ origin: [
       "http://localhost:5173",
-      "https://assignment-sales-studio-frontend.onrender.com/"
+      "https://assignment-sales-studio-frontend.onrender.com"
     ], 
     credentials: true 
   })
